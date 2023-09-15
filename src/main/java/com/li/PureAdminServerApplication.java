ac39c0a03ee4801e0738f79a361afe0a23b601ad
Echo-Eds <lizeyuan_xiaoli@qq.com>
2023-09-15 23:58:39 +0800

    测试提交

---
+++ src/main/java/com/li/PureAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.li;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureAdminServerApplication.class, args);
+	}
+
+}
